28be30d55a4a60bdffd89071a51e50689ad5398f
Naomi Dennis <njdennis94@gmail.com>
2018-11-05 17:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 let info = {aIssues : issues }
 let src = document.getElementById("issue-log").innerHTML 
 let template = Handlebars.compile(src)
+
+
 document.getElementById("main").innerHTML += template(src) 
 
